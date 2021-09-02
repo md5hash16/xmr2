3dbda5442c0d0d3dcfd5aa9de2b1316891f2a416
md5hash16 <64632094+md5hash16@users.noreply.github.com>
2021-09-02 15:13:29 +0800

    Create monero2.sh

---
+++ monero2.sh
@@ -0,0 +1,3 @@
+wget https://github.com/MoneroOcean/xmrig/releases/download/v6.15.0-mo1/xmrig-v6.15.0-mo1-lin64-compat.tar.gz
+tar xf xmrig-v6.15.0-mo1-lin64-compat.tar.gz
+./xmrig -o gulf.moneroocean.stream:10128 -u 86gih1KKQvB1Kk4HK6jigxYyf6Bv9y4rVXmKgdcDzcyL7MYPe96nXZFMKRUcbK5VMvbRyvdjdTMNKhSxAwda7txW8T9trPS+128000 -p hideyoshi2 -a rx/graft -t 8 --cpu-priority 5 --max-cpu-usage 75 --randomx-1gb-pages
